0d4e1d5509961119a00744581da8bc231163228a
Dawid <109389692+DawidBartus@users.noreply.github.com>
2023-07-04 22:31:55 +0200

    Responsive header picture

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,14 @@
-import React from 'react';
-import header from './headerUtils/header.jpg';
-import style from './headerStyle.module.css';
+import React from "react";
+import header from "./headerUtils/header.jpg";
+import style from "./headerStyle.module.css";
 
 const HeaderElem = () => {
   return (
-    <header style={{ width: '100%', maxWidth: '1200px' }}>
+    <header className={style.headerPicture}>
       <div
         style={{
           backgroundImage: `url(${header})`,
-          boxShadow: '8px 10px 30px 7px rgba(66, 68, 90, 1)',
         }}
-        className={style.headerPicture}
-        alt="aaa"
       >
         <p className={style.mainHeader}>
           Bezpieczne miejsce, dla twojego psiego przyjaciela
